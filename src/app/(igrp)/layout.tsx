ccc763489738a366236c5715fb280f0097862577
NOSI / PM - Carlos Alberto Andrade  Graca <carlosa.graca@nosi.cv>
2025-10-23 14:51:50 -0100

    Update layout configuration handling in RootLayout to enforce type safety by casting layoutConfig to IGRPLayoutConfigArgs. This change improves clarity and maintainability of the layout setup.

--- src/app/(igrp)/layout.tsx
+++ src/app/(igrp)/layout.tsx
@@ -4,10 +4,11 @@ import { headers } from 'next/headers';
 
 import { configLayout } from '@/actions/igrp/layout';
 import { createConfig } from '@igrp/template-config';
+import { IGRPLayoutConfigArgs } from '@igrp/framework-next-types';
 
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   const layoutConfig = await configLayout();
-  const config = await createConfig(layoutConfig);
+  const config = await createConfig(layoutConfig as IGRPLayoutConfigArgs);
 
   // TDOD: see to move this to the root-layout
   const { layout, previewMode } = config;
